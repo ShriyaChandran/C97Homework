07f03ed1961f6fc424e37aada9a39913de02a054
ShriyaChandran <62100109+ShriyaChandran@users.noreply.github.com>
2021-06-20 20:20:57 +0530

    Add files via upload

--- Homework97.py
+++ Homework97.py
@@ -1,14 +1,20 @@
+import random
 print("The number guessing game")
 guessCount=0
 number=random.randint(1,9)
-introString=input("Enter a number from 1 to 9")
-if(introString>number):
-    print("The number is lesser than")
-    print(number)
-    introString=input("Enter a number from 1 to 9")
-elif(introString<number):
-    print("The correct number is greater than ")
-    print(number)
-    introString=input("Enter a number from 1 to 9")
-else(introString==number):
-    print:Congratulations!! You guessed it man
+introString=int(input("Enter a number from 1 to 9"))
+while(guessCount<6):
+
+    if(introString>number):
+        print("The number is lesser than")
+        print(introString)
+        introString=int(input("Enter a number from 1 to 9"))
+    elif(introString<number):
+        print("The correct number is greater than ")
+        print(introString)
+        introString=int(input("Enter a number from 1 to 9"))
+    else:
+        print("Congratulations!! You guessed it man")
+    guessCount=guessCount+1
+if(guessCount==6):
+    print("You lose. sad.")
